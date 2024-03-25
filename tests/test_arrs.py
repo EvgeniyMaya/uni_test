eedfc2284a46ecdfb31525011b45f98ad095d495
EvgeniyMaya <super89048368898@yandex.ry>
2024-03-25 08:16:45 +0400

    unitest_test 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,12 +2,19 @@ from utils import arrs
 
 import unittest
 
+from utils import arrs
+
+import unittest
+
 class Uni(unittest.TestCase):
     def test_get(self):
-        assert arrs.get([1, 2, 3], 1, "test") == 2
-        assert arrs.get([], 0, "test") == "test"
+        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
+        self.assertEqual(arrs.get([], 0, "test"), "test")
 
 
     def test_slice(self):
-        assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-        assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([], 1, 2), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -1), [3])
+        self.assertEqual(arrs.my_slice([1, 2, 3], None), [1, 2, 3])
